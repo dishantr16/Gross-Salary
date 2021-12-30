2ed9ee04bfc47d4b8783d1ae57181d075c01914e
Sourcery AI <>
2021-12-30 17:38:14 +0000

    'Refactored by Sourcery'

--- grossSalary.py
+++ grossSalary.py
@@ -1,6 +1,6 @@
-for i in range(int(input())):
+for _ in range(int(input())):
     a=int(input())
-    if(a<1500):
+    if (a<1500):
         print(float(a+a*0.10+a*0.90))
-    elif(a>=1500):
+    else:
         print(float(a+500+0.98*a))
